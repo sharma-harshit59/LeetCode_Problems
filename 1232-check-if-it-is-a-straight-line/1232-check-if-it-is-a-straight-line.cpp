7e8ce63d19b699a20ddadc8d2f5e69938dbbbafa
Harshit Sharma <sharma.harshit0059@gmail.com>
2022-07-10 12:13:07 +0530

    Time: 18 ms (31.11%), Space: 10.1 MB (42.00%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& points) {
+        sort(points.begin(), points.end());
+        int x1, x2, x3, y1, y2, y3;
+        for (int i = 2; i < points.size(); i++) {
+            x1 = points[i-2][0];
+            x2 = points[i-1][0];
+            x3 = points[i][0];
+            y1 = points[i-2][1];
+            y2 = points[i-1][1];
+            y3 = points[i][1];
+            
+            if (((y2 - y1) * (x3 - x2)) != ((y3 - y2) * (x2 - x1)))
+                return false;
+        }
+        return true;
+    }
+};
