7bf949a42c575122d83a122342e9a8d06f46f03b
Harshit Sharma <sharma.harshit0059@gmail.com>
2022-07-14 12:44:12 +0530

    Time: 3 ms (43.44%), Space: 6.2 MB (7.85%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (int i = 0; i < s.size(); i++)
+            if (('A' <= s[i]) && (s[i] <= 'Z'))
+                s[i] += 32;
+        return s;
+    }
+};
