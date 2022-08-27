1c5b5e6d3d1c6f48283a7cbefbf038fcb8823be3
Harshit Sharma <sharma.harshit0059@gmail.com>
2022-08-27 10:03:59 +0530

    Time: 20 ms (27.76%), Space: 14.3 MB (25.01%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -5,14 +5,12 @@ public:
             return image;
         int m = image.size(), n = image[0].size();
         queue<pair<int, int>> q;
-        set<pair<int, int>> visited;
         q.push({sr, sc});
         int scolor = image[sr][sc], ti, tj;
         while (!q.empty()) {
             ti = q.front().first;
             tj = q.front().second;
             image[ti][tj] = color;
-            visited.insert({ti, tj});
             q.pop();
             if ((0 <= ti-1) && (image[ti-1][tj] == scolor))
                 q.push({ti-1, tj});
