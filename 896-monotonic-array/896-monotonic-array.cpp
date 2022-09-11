e03a5dce6d4c127b36551fda624b4b38622ac6ba
Harshit Sharma <sharma.harshit0059@gmail.com>
2022-09-11 09:26:49 +0530

    Time: 191 ms (87.24%), Space: 96.4 MB (65.83%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        if (nums.size() < 3)
-            return true;
-        int flag = 0;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] > nums[i-1]) {
-                if (flag == 0) flag = 1;
-                else if (flag == -1) return false;
-                else;
-            }
-            else if (nums[i] < nums[i-1]) {
-                if (flag == 0) flag = -1;
-                else if (flag == 1) return false;
-                else;
-            }
-            else;
-        }
-        return true;
+        int flag = (nums[0] <= nums[nums.size()-1]) ? 1 : -1;
+        for (int i = 1; (i < nums.size()) && (flag == 1); i++)
+            if (nums[i] < nums[i-1])
+                flag = 0;
+        for (int i = 1; (i < nums.size()) && (flag == -1); i++)
+            if (nums[i] > nums[i-1])
+                flag = 0;
+        return (flag) ? true : false;
     }
 };
