4a83b7d03270a5079de42e1042e5ea222422fb80
Harshit Sharma <sharma.harshit0059@gmail.com>
2022-09-19 20:28:24 +0530

    Time: 3 ms (65.99%), Space: 8.7 MB (85.35%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size(), carry = 1, temp;
-        for (int i = n-1; i >= 0 && carry; i--) {
-            temp = (digits[i] + carry);
-            carry = (temp / 10);
-            digits[i] = (temp % 10);
+        int carry = 1;
+        for (int i = digits.size()-1; i >= 0 && carry; i--) {
+            digits[i] += carry;
+            carry = digits[i] / 10;
+            digits[i] %= 10;
         }
         if (carry)
             digits.insert(digits.begin(), carry);
