a35199759dbdb0ab51f86ce4f40ba86c273000cf
Harshit Sharma <sharma.harshit0059@gmail.com>
2022-09-25 19:08:25 +0530

    Time: 166 ms (28.46%), Space: 19.1 MB (5.16%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -13,7 +13,7 @@ public:
     ListNode* merge(ListNode* list1, ListNode* list2) {
         ListNode *res = new ListNode(), *temp = res;
         int temp_val;
-        while (list1 != nullptr && list2 != nullptr) {
+        while (list1 && list2) {
             if ((list1 -> val) < (list2 -> val)) {
                 temp_val = list1 -> val;
                 list1 = list1 -> next;
@@ -25,16 +25,10 @@ public:
             temp -> next = new ListNode(temp_val);
             temp = temp -> next;
         }
-        while (list1 != nullptr) {
-            temp -> next = new ListNode(list1 -> val);
-            list1 = list1 -> next;
-            temp = temp -> next;
-        }
-        while (list2 != nullptr) {
-            temp -> next = new ListNode(list2 -> val);
-            list2 = list2 -> next;
-            temp = temp -> next;
-        }
+        if (list1)
+            temp -> next = list1;
+        if (list2)
+            temp -> next = list2;
         return res -> next;
     }
     
