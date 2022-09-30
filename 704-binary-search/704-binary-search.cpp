5e92b8f027d9b66553b1d8f340815b58b47c546b
Harshit Sharma <sharma.harshit0059@gmail.com>
2022-09-30 14:15:42 +0530

    Time: 118 ms (6.44%), Space: 27.5 MB (63.19%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        /*
+            The variables defined below are:
+            low:  stores the start-index of the search space array
+            high: stores the end-index of the search space array
+            mid:  stores the mid-index value of the search space array
+        */
         int low = 0, high = nums.size()-1, mid;
+        
+        /*
+            We loop until the search space vanishes, i.e., low is less
+            than or equal to high. If low is greater than high, this
+            means that our search space has vanished            
+        */
         while (low <= high) {
             mid = low + ((high - low) / 2);
+            /*
+                If the value at the mid-index is equal to the target we
+                return the index else, we reposition the low and high
+                values to half the search space
+            */
             if (nums[mid] == target)
                 return mid;
             else if (nums[mid] < target)
@@ -11,6 +28,11 @@ public:
             else
                 high = mid-1;
         }
+        /*
+            If no mid-index is found such that the value present at the
+            index is equal to the target value, this shows that the target
+            value is not present in the array, hence we return -1
+        */
         return -1;
     }
 };
