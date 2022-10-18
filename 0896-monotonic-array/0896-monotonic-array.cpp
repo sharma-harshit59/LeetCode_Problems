3868b42d704f45a826164be69a12bd1c40f3c10e
Harshit Sharma <sharma.harshit0059@gmail.com>
2022-10-18 08:53:06 +0530

    Time: 173 ms (96.69%), Space: 96.6 MB (18.95%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        /* The variable 'flag' has been used to keep track of any changes
+           encountered while traversing the array to check for Monotonicity
+           Here, we have checked the first and last element to check the type of Monotonicity */
+        int flag = (*nums.begin() <= *nums.rbegin()) ? 1 : -1;
+        
+        /* Check if the Array is Monotonically Increasing */
+        for (int i = 1; (i < nums.size()) && (flag == 1); i++)
+            if (nums[i] < nums[i-1])
+                flag = 0;
+        
+        /* Check if the Array is Monotonically Decreasing */
+        for (int i = 1; (i < nums.size()) && (flag == -1); i++)
+            if (nums[i] > nums[i-1])
+                flag = 0;
+        return flag;
+    }
+};
