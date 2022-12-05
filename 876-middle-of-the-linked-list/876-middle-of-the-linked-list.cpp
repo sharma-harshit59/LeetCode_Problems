4be9eceb7d44040101f9f4de5297cda9fa3b7108
Harshit Sharma <sharma.harshit0059@gmail.com>
2022-12-05 09:36:25 +0530

    Update 876-middle-of-the-linked-list.cpp

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -11,13 +11,23 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode *slow = head, *fast = head;
-        while (((fast -> next) != nullptr) && ((fast -> next -> next) != nullptr)) {
-            slow = (slow -> next);
+        /*
+        We define two pointers - fast & slow, where fast will move two pointers
+        ahead everytime (if possible), and slow will move one pointer ahead everytime
+        */
+        ListNode *fast = head, *slow = head;
+        while ((fast -> next) && (fast -> next -> next)) { // As we have to move two pointers ahead, we have to check both conditions
             fast = (fast -> next -> next);
-        }
-        if ((fast -> next) != nullptr)
             slow = (slow -> next);
+        }
+        /*
+        The above loop return the first middle node, as for even number of nodes
+        the fast pointer ends at the second last node, which leads us to slow at
+        the first middle node. So, we position the slow one pointer ahead, if the
+        fast pointer is at the second last node
+        */
+        if (fast -> next)
+            slow = slow -> next;
         return slow;
     }
 };
